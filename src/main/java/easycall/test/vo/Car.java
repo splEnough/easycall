c3ad69e8e64ba7c0a314d2660d1a0a4d3c5ac7ef
翁富鑫 <18783278226@163.com>
2019-04-04 18:48:19 +0800

    提交代码

---
+++ src/main/java/easycall/test/vo/Car.java
@@ -0,0 +1,7 @@
+package easycall.test.vo;
+
+/**
+ * @author 翁富鑫 2019/3/30 16:16
+ */
+public class Car {
+}
---
+++ src/main/java/easycall/test/vo/City.java
@@ -0,0 +1,7 @@
+package easycall.test.vo;
+
+/**
+ * @author 翁富鑫 2019/3/30 16:06
+ */
+public class City {
+}
